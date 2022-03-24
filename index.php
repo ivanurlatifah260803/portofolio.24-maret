162af812736d68c3b6ca6d52cfbc06f19b97d9b3
ivanurlatifah260803 <97926547+ivanurlatifah260803@users.noreply.github.com>
2022-03-24 15:45:31 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+//menghubungkan ke file init.php yang ada difolder admin
+require_once '../admin/init.php';
+
+$app=new App;
+
---
+++ init.php
@@ -0,0 +1,11 @@
+<?php
+//menghubungkan ke file App.php
+
+//menghubungkan ke App.php yang mengatur URL
+require_once 'once/App.php';
+
+//menghubungkan ke Controller utama
+require_once 'core/controller.php';
+
+//menghubungkan ke file konfigurasi
+require_once 'config/Config.php'
